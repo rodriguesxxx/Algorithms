4895c52a5d30484e44cba3e5a19003285b946f0e
blkd <blkd.html@gmail.com>
2023-09-01 10:51:44 -0300

    .

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
     public static void main(String[] args ){
         LinkedList linkedList = new LinkedList();
+        linkedList.add("Daniel Rodrigues");
         linkedList.add(12);
-        linkedList.add(12);
-        linkedList.add("12");
+        linkedList.add(2.3);
         System.out.println(linkedList.lenght());
         System.out.println(linkedList.getAll());
 
