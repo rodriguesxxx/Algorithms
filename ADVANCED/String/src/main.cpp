c628ecdc531d223daf20b91b5336d26751aa5a7a
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-02-02 22:22:13 -0400

    [ADVANCED] implementing string data structure from 0

---
+++ ADVANCED/String/src/main.cpp
@@ -0,0 +1,10 @@
+#include "string.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    String str;
+    cin >> str;
+    cout << str << endl;
+    return  0;
+}
---
+++ ADVANCED/String/src/string.hpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+
+#ifndef STRING_H
+#define STRING_H
+
+class String {
+
+    private:
+        char *str;
+        
+        void setStr(const char *s) {
+            str = new char[strlen(s) + 1];
+            strcpy(str, s);
+        }
+
+        char* getStr() {
+            return str;
+        }
+
+    public:
+
+        String(const char *s = "") {
+            this->setStr(s);
+            strcpy(str, s);
+        }
+
+        friend std::ostream& operator<<(std::ostream &out, const String &s) {
+            out << s.str;
+            return out;
+        }
+
+        friend std::istream& operator>>(std::istream &in, String &s) {
+            char buffer[256];
+            in >> buffer;
+            delete[] s.str;
+            s.setStr(buffer);
+            return in;
+        }
+
+        ~String() {
+            delete[] str;
+        }
+};
+
+#endif
