c41d60ac0c2a067cc28a64d88390002197e65d88
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2025-01-13 15:07:20 -0300

    [MEDIUM] System of Linear Equations: [FEAT] start of processing user input

--- MEDIUM/LinearEquations/solution.cpp
+++ MEDIUM/LinearEquations/solution.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<sstream>
+#include<cctype>
+#include<algorithm>
 
 using namespace std;
 
 struct LinearEquation {
     int a1, b1, a2, b2, r1, r2;
 };
+
+void process_expression(const string &expression, int &coefX, int &coefY) {
+    coefX = coefY = 1;
+
+    istringstream stream(expression);
+    string term;
+    char last = '\0';
+
+    while(stream >> term) {
+        
+        term.erase(remove(term.begin(), term.end(), ' '), term.end());
+
+        int sign = (term[1] == '-') ? -1 : 1;
+       
+        cout << term << endl;
+    }
+
+    cout << coefX << endl << coefY << endl;
+}
 
 int calc_determinant(const int a1, const int b1, const int a2, const int b2) {
     const int d = a1 * b2 + (a2 * b1) * -1;
@@ -29,12 +52,19 @@ vector<int> solve_systeam(const LinearEquation &eq) {
 
 int main() {
 
-    const LinearEquation eq = {2, 1, 1, 3, 10, 15};
+    int coefX, coefY;
+    
+    string expression;
+    cout << "Digite a expressão (ex: 2x + y): ";
+    getline(cin, expression);
 
-    const vector<int> sXY = solve_systeam(eq);
+    process_expression(expression, coefX, coefY);
 
-    cout << "x = " << sXY[0] << endl;
-    cout << "y = " << sXY[1] << endl;
+    // const LinearEquation eq = {2, 1, 1, 3, 10, 15};
+    // const vector<int> sXY = solve_systeam(eq);
+
+    // cout << "x = " << sXY[0] << endl;
+    // cout << "y = " << sXY[1] << endl;
     
     return 0;
 }
