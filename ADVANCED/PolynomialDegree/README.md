b07a5687b99205a008a908dd0184e8d747ad281a
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2025-02-01 17:32:48 -0300

    [Polynomial Degree]: Planning

---
+++ ADVANCED/PolynomialDegree/README.md
@@ -0,0 +1,21 @@
+create a library of the algorithm done in #19, use this library to determine the degree of a polynomial.
+
+**_The main objective of this algorithm is to create libraries_**
+
+**input:**
+
+```bash
+3x + x^3 - 4 + 3x^3
+```
+
+**_use the library to process and order the polynomial, the return will be an array with all the exponents, from this, determine the degree._**
+
+```bash
+#return: [3, 1, 0]
+```
+
+**output:**
+
+```bash
+4 degree
+```
---
+++ ADVANCED/PolynomialDegree/polynomial.cpp
@@ -0,0 +1,20 @@
+/*
+    TODO: Converter código da issue(#19) para C++
+    TODO: Criar bibloteca(polynomials)
+        //FUNC PUBLICAS
+            //TODO: order(func para ordenar polinomio)
+            //TODO: degree(func que retorna o grau do polinomio)
+            //TODO: degrees(func que retorna todos os graus do polinomio)
+
+        //FUNC PRIVADAS
+            TODO: sort(ordenar graus dos polinomios de forma decrescente)(merge sort)
+*/
+
+
+int degree(char *expression) {
+    return -1;
+}
+
+int main() {
+    return 0;
+}
