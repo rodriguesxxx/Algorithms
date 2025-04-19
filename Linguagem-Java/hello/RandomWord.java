8f0eea005d177f556e56ef06e523a3949ce5d04d
Daniel <drp6@aluno.ifnmg.edu.br>
2025-04-19 17:25:17 -0300

    [Linguagem-Java]: [PRINCETON COURSE] Exercicios módulo 1

---
+++ Linguagem-Java/hello/RandomWord.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = null;
+        int count = 0;
+
+        while (!StdIn.isEmpty()){
+            String word = StdIn.readString();
+            count++;
+
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champion = word;
+            }
+        }
+
+        if (champion != null) {
+            StdOut.println(champion);
+        }
+    }
+}
